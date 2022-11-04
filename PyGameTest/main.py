a64a60cc2166bef421fcc96769f51a0c5634baa0
R1fty0 <113545024+R1fty0@users.noreply.github.com>
2022-11-03 17:33:49 -0700

    Movement is semi-complete. Asteroids need to be spawned in before weapon controls are implemented. World Wrapping also needs to be implemented.
    List of things to do:
    
    1. World Wrapping
    
    2. Asteroid Spawning
    
    3. Teleport Jump
    
    4. Laser Weapon
    
    5. Missile Weapon
    
    6. Fireball Weapon
    
    7. Fire rate
    
    8. Projectile Collisions
    
    9. Score Keeping
    
    10. Main Menu and UI
    
    11. Sounds

--- PyGameTest/main.py
+++ PyGameTest/main.py
@@ -11,6 +11,7 @@ WIDTH, HEIGHT = 1000, 500
 # Images - Goes through the file's directory via the operating system to access this image.
 ORIGINAL_SPACESHIP_IMAGE = pygame.image.load(os.path.join("venv", "spaceship.png"))
 ORIGINAL_BACKGROUND_IMAGE = pygame.image.load(os.path.join("venv", "background.jpg"))
+ORIGINAL_LASER_IMAGE = pygame.image.load(os.path.join("venv", "shiplaser.png"))
 
 
 # Spaceship Dimensions
@@ -19,12 +20,20 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 58, 96
 # Scales Images
 SPACESHIP_IMAGE = pygame.transform.scale(ORIGINAL_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 BACKGROUND_IMAGE = pygame.transform.scale(ORIGINAL_BACKGROUND_IMAGE, (WIDTH, HEIGHT))
+LASER_IMAGE = pygame.transform.scale(ORIGINAL_LASER_IMAGE, (SPACESHIP_WIDTH/2, SPACESHIP_HEIGHT/2))
 
 
 # Creates Window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 # Names the title of the program
 pygame.display.set_caption("Asteroids V1.0")
+
+# Speed of the Spaceship
+SPEED = 5
+# Bullet Speed
+BULLET_SPEED = 5
+# Max Ammo
+MAX_AMMO = 5
 
 
 # Draws Stuff on the Screen
@@ -36,10 +45,31 @@ def draw(spaceship):
     pygame.display.update()
 
 
+# Handles Player Movement
+def movement(spaceship):
+    # Similar to Unity's "GetKey" or Greenfoot's "GetKeyDown"
+    key_down = pygame.key.get_pressed()
+
+    if key_down[pygame.K_a]:
+        # Moves Player to the Right
+        spaceship.x -= SPEED
+    if key_down[pygame.K_d]:
+        # Moves Player to the Left
+        spaceship.x += SPEED
+    if key_down[pygame.K_w]:
+        # Moves Player Forward
+        spaceship.y -= SPEED
+    if key_down[pygame.K_s]:
+        # Moves Player Backwards
+        spaceship.y += SPEED
+
+
 # Same as the Unity Update or Greenfoot Act Method
 def main():
     # Sets Player Position - Arguments for pygame.Rect() = x, y, width, height.
     spaceship = pygame.Rect(500, 250, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    bullets = []
 
     # Clock that maintains FPS
     clock = pygame.time.Clock()
@@ -51,9 +81,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        # Similar to Unity's "GetKey" or Greenfoot's "GetKeyDown"
-        key_down = pygame.key.get_pressed()
-        # if key_down[pygame.K_a]: # Left Key
+            # Handles Shooting
+            # if event.type == pygame.KEYDOWN:
+                # if event.key == pygame.K_LCTRL and len(bullets) < MAX_AMMO:
+                # bullets.append(laser)
+        # handle_weapons(bullets)
+        movement(spaceship)
         draw(spaceship)
 
     # Quits program
